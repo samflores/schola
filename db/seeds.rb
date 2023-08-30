efec77096956ebfae1f3607b2f55d6c6af4ed25c
NehamaMandelbaum <nehama1836@gmail.com>
2023-08-30 10:48:35 -0300

    optimizing seed performance

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,27 +31,48 @@ courses2012.each do |course|
   rand(20..40).times do
     course.enrollments.create!(code: SecureRandom.uuid, student: FactoryBot.create(:student))
   end
+end
 
-  subjects.each do |subject|
-    course.teacher_assignments.create!(course:, subject:, teacher: teachers.sample)
+teacher_assignments = courses2012.map do |course|
+  subjects.map do |subject|
+    { course_id: course.id, subject_id: subject.id,
+      teacher_id: teachers.sample.id }
   end
 end
 
-Enrollment.all.map do |enrollment|
+TeacherAssignment.insert_all(teacher_assignments.flatten)
+
+exams2012 = courses2012.map do |course|
   subjects.map do |subject|
-    8.times do
-      Grade.new(value: rand(0..10), enrollment:,
-                exam: Exam.new(course: enrollment.course, subject:, realized_on: FFaker::Time.between(Date.new(2012, 1, 1), Date.new(2012, 12, 31)).to_date))
+    { course_id: course.id, subject_id: subject.id,
+      realized_on: FFaker::Time.between(Date.new(2012, 1, 1), Date.new(2012, 12, 31)).to_s }
+  end
+end
+
+Exam.insert_all(exams2012.flatten)
+
+grades2012 = courses2012.map do |course|
+  course.enrollments.map do |enrollment|
+    course.exams.map do |exam|
+      exams = []
+      8.times do
+        exams.push({ value: rand(0..10), enrollment_code: enrollment.code, exam_id: exam.id })
+      end
+      exams
     end
   end
 end
 
+Grade.insert_all(grades2012.flatten)
+
 # Para 2013:
 
 # Criando os cursos de 2013
-(5..9).to_a.each do |grade|
-  Course.create(year: 2013, name: "#{grade}º ano", starts_on: '2013-01-01', ends_on: '2013-12-31')
+courses2013 = (5..9).to_a.map do |grade|
+  { year: 2013, name: "#{grade}º ano", starts_on: '2013-01-01', ends_on: '2013-12-31' }
 end
+
+Course.insert_all(courses2013)
 
 # Matriculando alunos novos no 5º ano
 rand(20..40).times do
@@ -60,28 +81,44 @@ rand(20..40).times do
 end
 
 # Criando matrícula do curso seguinte para os alunos do 5º ao 8º ano:
-(5..8).to_a.each do |year|
-  Course.where(year: 2012, name: "#{year}º ano").first.students.each do |student|
-    Enrollment.create!(code: SecureRandom.uuid, student:,
-                       course: Course.where(year: 2013, name: "#{year + 1}º ano").first)
+enrollments2013 = (5..8).to_a.map do |year|
+  Course.where(year: 2012, name: "#{year}º ano").first.students.map do |student|
+    { code: SecureRandom.uuid, student_id: student.id,
+      course_id: Course.where(year: 2013, name: "#{year + 1}º ano").first.id }
   end
 end
+
+Enrollment.insert_all(enrollments2013.flatten)
 
 courses2013 = Course.where(year: 2013)
 
-courses2013.each do |course|
-  subjects.each do |subject|
-    course.teacher_assignments.create!(course:, subject:, teacher: teachers.sample)
+teacher_assignments2013 = courses2013.map do |course|
+  subjects.map do |subject|
+    { course_id: course.id, subject_id: subject.id, teacher_id: teachers.sample.id }
   end
 end
 
-Course.where(year: 2013).each do |course|
-  course.enrollments.each do |enrollment|
-    subjects.each do |subject|
+TeacherAssignment.insert_all(teacher_assignments2013.flatten)
+
+exams2013 = courses2013.map do |course|
+  subjects.map do |subject|
+    { course_id: course.id, subject_id: subject.id,
+      realized_on: FFaker::Time.between(Date.new(2013, 1, 1), Date.new(2013, 12, 31)).to_s }
+  end
+end
+
+Exam.insert_all(exams2013.flatten)
+
+grades2013 = courses2013.map do |course|
+  course.enrollments.map do |enrollment|
+    course.exams.map do |exam|
+      exams = []
       8.times do
-        Grade.create!(value: rand(0..10), enrollment:,
-                      exam: Exam.new(course: enrollment.course, subject:, realized_on: FFaker::Time.between(Date.new(2012, 1, 1), Date.new(2012, 12, 31)).to_date))
+        exams.push({ value: rand(0..10), enrollment_code: enrollment.code, exam_id: exam.id })
       end
+      exams
     end
   end
 end
+
+Grade.insert_all(grades2013.flatten)
